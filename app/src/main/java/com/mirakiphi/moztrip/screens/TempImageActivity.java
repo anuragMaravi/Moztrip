1fb7152dc3c9b85f6453ce40af6cfd260ef04d2e
KwinBobby <kwinbobby@gmail.com>
2017-04-07 16:09:06 +0530

    Added a new activity and passed a lot of extras

---
+++ app/src/main/java/com/mirakiphi/moztrip/screens/TempImageActivity.java
@@ -0,0 +1,16 @@
+package com.mirakiphi.moztrip.screens;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mirakiphi.moztrip.R;
+
+
+public class TempImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_temp_image);
+    }
+}
