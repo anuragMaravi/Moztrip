f10fcbf25c9be749be7ef5b210c32c8f36954dcc
anuragMaravi <anuragmaravi12@gmail.com>
2017-04-07 15:16:20 +0530

    Search Added

--- app/src/main/java/com/mirakiphi/moztrip/screens/MainActivity.java
+++ app/src/main/java/com/mirakiphi/moztrip/screens/MainActivity.java
@@ -1,10 +1,18 @@
 package com.mirakiphi.moztrip.screens;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
 import com.gigamole.navigationtabstrip.NavigationTabStrip;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocomplete;
 import com.mirakiphi.moztrip.adapters.MainPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,5 +29,36 @@ public class MainActivity extends AppCompatActivity {
         final NavigationTabStrip navigationTabStrip = (NavigationTabStrip) findViewById(com.mirakiphi.moztrip.R.id.nts);
         navigationTabStrip.setTitles("HOW WE WORK", "WE WORK WITH");
         navigationTabStrip.setViewPager(viewPager);
+    }
+
+    public void findPlace(View view) {
+        try {
+            Intent intent = new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN).build(this);
+            startActivityForResult(intent, 1);
+        } catch (GooglePlayServicesRepairableException e) {
+        } catch (GooglePlayServicesNotAvailableException e) {
+
+        }
+    }
+
+    // A place has been received; use requestCode to track the request.
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+// retrive the data by using getPlace() method.
+                Place place = PlaceAutocomplete.getPlace(this, data);
+                Log.e("Tag", "Place: " + place.getAddress() + place.getPhoneNumber()+place.getLatLng());
+
+
+            } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
+                Status status = PlaceAutocomplete.getStatus(this, data);
+// TODO: Handle the error.
+                Log.e("Tag", status.getStatusMessage());
+
+            } else if (resultCode == RESULT_CANCELED) {
+// The user canceled the operation.
+            }
+        }
     }
 }
